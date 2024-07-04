826e1422ce8c57a1d4bd0f67cedc2f320b17f617
Jayanta Omen <jayanta.dn@gmail.com>
2024-07-04 15:42:23 +0530

    future builder

--- lib/entry_table.dart
+++ lib/entry_table.dart
@@ -7,25 +7,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 class EntryTable extends StatefulWidget {
   EntryTable({super.key});
 
-  SlotTile selectedSlot = SlotTile(id: "unknown id", buttonText: 'unknown slot'); 
-
   @override
   State<EntryTable> createState() => _EntryTableState();
 }
 
 class _EntryTableState extends State<EntryTable> {
-  @override
-  void initState() {
-    _fetchSelectedSlot();
-  }
 
-  Future<void> _fetchSelectedSlot() async {
+  Future<SlotTile> _fetchSelectedSlot() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? str = await prefs.getString("selectedSlot");
-
-    setState(() {
-      widget.selectedSlot = SlotTile.fromJson(jsonDecode(str!));
-    });
+    String? str = prefs.getString("selectedSlot");
+    return SlotTile.fromJson(jsonDecode(str!));
   }
 
   @override
@@ -33,7 +24,16 @@ class _EntryTableState extends State<EntryTable> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.selectedSlot.buttonText),
+        title: FutureBuilder<SlotTile>(
+          future: _fetchSelectedSlot(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Text(snapshot.data!.buttonText);
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.delete),
