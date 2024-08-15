9841b02b191cdb711be0a273925e38b18979ee46
Jayanta Omen <jayanta.dn@gmail.com>
2024-08-15 21:11:47 +0530

    laddu stock log

--- lib/laddu/stock_log.dart
+++ lib/laddu/stock_log.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:garuda/fb.dart';
 import 'package:garuda/laddu/laddu_datatypes.dart';
+import 'package:intl/intl.dart';
 
 class StockLog extends StatefulWidget {
   @override
@@ -10,12 +11,34 @@ class StockLog extends StatefulWidget {
 class _StockLogState extends State<StockLog> {
   List<LadduStock> logs = [];
 
+  Future<void> _futureInit() async {
+    logs = await FB().readLadduStocks();
+    logs.sort((a, b) => b.timestamp.compareTo(a.timestamp));
+  }
+
   Widget _getListViewStock() {
     return ListView.builder(
       itemCount: logs.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text("HI"),
+        String formattedDate =
+            DateFormat('dd-MM-yyyy').format(logs[index].timestamp);
+        String formattedTime =
+            DateFormat('HH:mm').format(logs[index].timestamp);
+
+        return Column(
+          children: [
+            ListTile(
+              title: Text("Date: $formattedDate \t Time: $formattedTime"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Sevakarta: ${logs[index].user}"),
+                  Text('Laddu packets added: ${logs[index].count}'),
+                ],
+              ),
+            ),
+            Divider(),
+          ],
         );
       },
     );
@@ -27,6 +50,17 @@ class _StockLogState extends State<StockLog> {
         appBar: AppBar(
           title: Text('Laddu stock log'),
         ),
-        body: _getListViewStock());
+        body: FutureBuilder<void>(
+          future: _futureInit(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return _getListViewStock();
+            }
+          },
+        ));
   }
 }
