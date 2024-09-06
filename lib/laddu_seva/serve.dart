0074c6f739a21453d358d4d0bcddc404dac8e93a
Jayanta Omen <jayanta.dn@gmail.com>
2024-09-06 21:03:05 +0530

    refactor: Refactor Serve widget to improve code structure and readability

--- lib/laddu_seva/serve.dart
+++ lib/laddu_seva/serve.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:garuda/const.dart';
 
 class Serve extends StatefulWidget {
   @override
-  _serve createState() => _serve();
+  _ServeState createState() => _ServeState();
 }
 
-class _serve extends State<Serve> {
-  List<TableRow> _tableRows = [];
-
+class _ServeState extends State<Serve> {
   @override
   initState() {
     super.initState();
@@ -17,127 +14,89 @@ class _serve extends State<Serve> {
   }
 
   Future<void> _refresh() async {
-    _tableRows.clear();
-
-    // add the seva tickets
-    Const().ticketAmounts.forEach((value) {
-      _tableRows.add(TableRow(
-        children: [
-          // label
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Pushpanjali $value",
-                  style: TextStyle(
-                    fontSize: 18.0, // Increase font size
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          // input field - number of tickets
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: '0',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          // number of laddu packs
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '0',
-                  style: TextStyle(
-                    fontSize: 18.0, // Increase font size
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ));
-    });
-
-    // add others
-    ["Others", "Missing"].forEach((value) {
-      _tableRows.add(TableRow(
-        children: [
-          // label
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  value,
-                  style: TextStyle(
-                    fontSize: 18.0, // Increase font size
-                  ),
-                ),
-              ),
-            ),
-          ),
-
-          // input field - number of tickets (empty)
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox.shrink(), // Empty cell
-              ),
-            ),
-          ),
-
-          // number of laddu packs (TextField)
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: '0',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ));
-    });
-
     setState(() {});
+  }
+
+  Widget _createTable() {
+    Table table = Table(
+      columnWidths: {
+        0: FixedColumnWidth(150.0),
+        1: FixedColumnWidth(80.0),
+        2: FlexColumnWidth(),
+      },
+      children: [],
+    );
+
+    // header row
+    TableRow header = TableRow(
+      children: [
+        TableCell(
+          child: Center(
+            child: Text(
+              'Seva',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        TableCell(
+          child: Center(
+            child: Text(
+              'Tickets',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        TableCell(
+          child: Center(
+            child: Text(
+              'Packs',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
+    );
+    table.children.add(header);
+
+    // seva ticket row
+    TableRow row = TableRow(
+      children: [
+        TableCell(
+          child: Center(
+            child: Text(
+              'Laddu',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+        TableCell(
+          child: Center(
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+        TableCell(
+          child: Center(
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+      ],
+    );
+    table.children.add(row);
+
+    return table;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Serve Laddu'),
+        title: Text('Serve laddu'),
       ),
       body: RefreshIndicator(
         onRefresh: _refresh,
@@ -146,106 +105,7 @@ class _serve extends State<Serve> {
         // If you use ListView.builder inside this, then the ListView here can be removed.
         child: ListView(
           children: [
-            // table of services
-            Padding(
-              padding: const EdgeInsets.all(
-                  8.0), // Adjust the padding value as needed
-              child: Table(
-                columnWidths: {
-                  0: FlexColumnWidth(
-                      2), // Adjust the flex value as needed to make the first column wider
-                },
-                border: null,
-                children: [
-                  // table header
-                  TableRow(
-                    children: [
-                      Text(
-                        'Seva',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0, // Adjust the font size as needed
-                        ),
-                      ),
-                      Text(
-                        'Tickets',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0, // Adjust the font size as needed
-                        ),
-                      ),
-                      Text(
-                        'Packs',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0, // Adjust the font size as needed
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  // table rows
-                  ..._tableRows,
-                ],
-              ),
-            ),
-
-            // total packs
-            SizedBox(
-                height: 16.0), // Add some space between the table and the text
-            Center(
-              child: Text(
-                'Total laddu packs = ',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24.0, // Large font size
-                ),
-              ),
-            ),
-
-            // note field
-            Padding(
-              padding: const EdgeInsets.all(
-                  8.0), // Adjust the padding value as needed
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Note',
-                ),
-              ),
-            ),
-
-            // button row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // delete button
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Add your delete logic here
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          WidgetStateProperty.all<Color?>(Colors.red),
-                    ),
-                    child: Text('Delete'),
-                  ),
-                ),
-
-                // serve button
-                SizedBox(
-                    width:
-                        10), // Optional: Add some spacing between the buttons
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Add your serve logic here
-                    },
-                    child: Text('Serve'),
-                  ),
-                ),
-              ],
-            )
+            _createTable(),
           ],
         ),
       ),
