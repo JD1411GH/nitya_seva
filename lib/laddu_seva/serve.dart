fd8ae373d4c785f35b281cd8fdf03a55a6a5f92b
Jayanta Omen <jayanta.dn@gmail.com>
2024-09-06 22:29:49 +0530

    Refactor Serve widget to use a list for laddu pack quantities

--- lib/laddu_seva/serve.dart
+++ lib/laddu_seva/serve.dart
@@ -13,16 +13,105 @@ class _ServeState extends State<Serve> {
   void initState() {
     super.initState();
     _refresh();
-    _initializeControllers();
-  }
-
-  void _initializeControllers() {
-    _controllers = List.generate(
-        Const().ticketAmounts.length, (index) => TextEditingController());
   }
 
   Future<void> _refresh() async {
+    _controllers = List.generate(
+        Const().ticketAmounts.length, (index) => TextEditingController());
+
     setState(() {});
+  }
+
+  Widget _createTable() {
+    return Table(
+      columnWidths: {
+        0: FixedColumnWidth(
+            150.0), // Set the desired width for the first column
+      },
+      children: [
+        // table header
+        TableRow(
+          children: [
+            // seva header
+            TableCell(
+              child: Center(
+                child: Text(
+                  'Seva',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0, // Increase the font size as needed
+                  ),
+                ),
+              ),
+            ),
+
+            // tickets header
+            TableCell(
+              child: Center(
+                child: Text(
+                  'Tickets',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0, // Increase the font size as needed
+                  ),
+                ),
+              ),
+            ),
+
+            // packs header
+            TableCell(
+              child: Center(
+                child: Text(
+                  'Packs',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0, // Increase the font size as needed
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        // seva rows
+        for (int i = 0; i < Const().ticketAmounts.length; i++)
+          TableRow(
+            children: [
+              // seva name
+              TableCell(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Pushpanjali ${Const().ticketAmounts[i]}'),
+                ),
+              ),
+
+              // ticket count
+              TableCell(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextField(
+                    controller: _controllers[i],
+                    onChanged: (value) {
+                      setState(() {});
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+
+              // pack count
+              TableCell(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(_controllers[i].text),
+                ),
+              ),
+            ],
+          ),
+      ],
+    );
   }
 
   @override
@@ -35,42 +124,7 @@ class _ServeState extends State<Serve> {
         onRefresh: _refresh,
         child: ListView(
           children: [
-            Table(
-              border: TableBorder.all(),
-              children: [
-                TableRow(
-                  children: [
-                    TableCell(child: Center(child: Text('Seva'))),
-                    TableCell(child: Center(child: Text('Tickets'))),
-                    TableCell(child: Center(child: Text('Packs'))),
-                  ],
-                ),
-                for (int i = 0; i < Const().ticketAmounts.length; i++)
-                  TableRow(
-                    children: [
-                      TableCell(
-                          child: Center(
-                              child: Text(
-                                  'Pushpanjali ${Const().ticketAmounts[i]}'))),
-                      TableCell(
-                        child: Center(
-                          child: TextField(
-                            controller: _controllers[i],
-                            onChanged: (value) {
-                              setState(() {});
-                            },
-                          ),
-                        ),
-                      ),
-                      TableCell(
-                        child: Center(
-                          child: Text(_controllers[i].text),
-                        ),
-                      ),
-                    ],
-                  ),
-              ],
-            ),
+            _createTable(),
           ],
         ),
       ),
