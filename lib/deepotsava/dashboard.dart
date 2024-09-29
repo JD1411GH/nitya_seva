e8a49fb76f3620c2b6f09734a018af4cb2b7a2dd
Jayanta Omen <jayanta.dn@gmail.com>
2024-09-29 20:30:53 +0530

    placeholder for mode chart

--- lib/deepotsava/dashboard.dart
+++ lib/deepotsava/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garuda/deepotsava/mode_chart.dart';
 import 'package:synchronized/synchronized.dart';
 
 class Dashboard extends StatefulWidget {
@@ -33,16 +34,14 @@ class _DashboardState extends State<Dashboard> {
   }
 
   Widget _createLampCount(double height) {
-    return Center(
-      child: Container(
-        height: height,
-        width: height,
-        child: CircleAvatar(
-          radius: height / 2,
-          child: Text(
-            '$_lampsIssued',
-            style: TextStyle(fontSize: 32.0),
-          ),
+    return Container(
+      height: height,
+      width: height,
+      child: CircleAvatar(
+        radius: height / 2,
+        child: Text(
+          '$_lampsIssued',
+          style: TextStyle(fontSize: 32.0),
         ),
       ),
     );
@@ -55,7 +54,38 @@ class _DashboardState extends State<Dashboard> {
     return SizedBox(
       height: height,
       child: Card(
-        child: _createLampCount(height),
+        child: Row(
+          children: [
+            // left side widget
+            Container(
+              height: height,
+              width: height,
+              color: Colors.grey[300], // Placeholder widget on the left
+              child: Center(
+                child: Text(
+                  'Placeholder',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+            ),
+
+            Spacer(),
+
+            // center widget
+            Center(
+              child: _createLampCount(height),
+            ),
+
+            Spacer(),
+
+            // right side widget
+            SizedBox(
+              height: height,
+              width: height,
+              child: ModeChart(),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/deepotsava/mode_chart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModeChart extends StatefulWidget {
+  const ModeChart({super.key});
+
+  @override
+  State<ModeChart> createState() => _ModeChartState();
+}
+
+class _ModeChartState extends State<ModeChart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
