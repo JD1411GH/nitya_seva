4c3ee75c2f3fbffb7088eecdd8b6530ed1e04d85
JD1411 <jyd1kor@bosch.com>
2024-10-22 13:37:53 +0530

    Refactor date header to display and update the selected date

--- lib/deepotsava/date_header.dart
+++ lib/deepotsava/date_header.dart
@@ -15,73 +15,70 @@ final GlobalKey<_DateHeaderState> HistoryHeaderKey =
 
 class _DateHeaderState extends State<DateHeader> {
   final _lockInit = Lock();
-  String date = "";
+  DateTime _date = DateTime.now();
 
-  Future<void> _futureInit() async {
-    await _lockInit.synchronized(() async {
-      date = DateFormat('EEE, MMM dd').format(DateTime.now());
-    });
+  @override
+  initState() {
+    super.initState();
+
+    refresh();
   }
 
   Future<void> refresh() async {
-    await _futureInit();
     if (!mounted) return;
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-      future: _futureInit(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // previous day button
-                IconButton(
-                  icon: Transform.rotate(
-                    angle: 3.14, // Rotate 180 degrees to point left
-                    child: Icon(Icons.play_arrow),
-                  ),
-                  onPressed: () {
-                    // Handle previous button click
-                  },
-                ),
-
-                // date
-                GestureDetector(
-                  onTap: () {
-                    // Handle text click
-                  },
-                  child: Text(
-                    date,
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ), // Adjust the font size as needed
-                  ),
-                ),
-
-                // next day button
-                IconButton(
-                  icon: Icon(Icons.play_arrow), // Default points right
-                  onPressed: () {
-                    // Handle next button click
-                  },
-                ),
-              ],
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // previous day button
+          IconButton(
+            icon: Transform.rotate(
+              angle: 3.14, // Rotate 180 degrees to point left
+              child: Icon(Icons.play_arrow),
             ),
-          );
-        }
-      },
+            onPressed: () {
+              setState(() {
+                _date = _date.subtract(Duration(days: 1));
+              });
+            },
+          ),
+
+          // date
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _date = DateTime.now();
+              });
+            },
+            child: Container(
+              width: 120.0,
+              alignment: Alignment.center,
+              child: Text(
+                DateFormat('EEE, dd MMM').format(_date),
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ), // Adjust the font size as needed
+              ),
+            ),
+          ),
+
+          // next day button
+          IconButton(
+            icon: Icon(Icons.play_arrow), // Default points right
+            onPressed: () {
+              setState(() {
+                _date = _date.add(Duration(days: 1));
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
